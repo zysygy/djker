427b90ba79cdff2c8799de9e128858cd2be96f89
zysygy <a37chen@uwaterloo.ca>
2016-02-07 23:43:58 -0500

    Revamped graph structure, finished core Dijkstra algo
    Changed vector of edge arrays to 2D vector to be able to leverage size member.
    I considered using a list; still need to do an analysis to determine what is technically
    more efficient.
    
    Core algo is also complete. Whole thing is untested right now.

--- djker.cpp
+++ djker.cpp
@@ -15,84 +15,135 @@ Pointers(?)
 #include <vector>
 #include <string>
 #include <iostream>
+#include <array>
+#include <stack>
 
 using namespace std;
 
 //This could (and should?) probably be a class with access functions, but let's worry about that later
-struct edge {
+typedef struct {
 	int dest;
 	int wt;
-};
+} edge;
 
 int main (){
 
 	//Set the endpoints of the algorithm
-	int start = 0;
-	int end = 0;
+	int startNode = 0;
+	int endNode = 0;
 	
 	//Graph is vector of edge lists (arrays)
+	//Vector and Lists each have their own +/-
 	//Each member of vector is a node in the graph
 	
-	vector<edge*> graph = vector<edge*>();
+	vector<vector<edge>> graph();
 	
 	////////////////////////////////
 	//Hard code the graph for now
 	//a1 = node 1, b2 = node 2, etc
-	edge a1[2];
-	a1[0].dest = 4;
-	a1[0].wt =  8;
-	a1[1].dest = 6;
-	a1[1].wt = 6;
+	
+	vector<edge> a1(2);
+	a1[0] = {4, 8}; // same as a1[0].dest = 4; a1[0].wt = 8;
+	a1[1] = {6, 6};
 	graph.push_back (a1);
 	
-	edge b2[2];
-	b2[0].dest = 3;
-	b2[0].wt =  10;
-	b2[1].dest = 4;
-	b2[1].wt = 4;
+	vector<edge> b2(2);
+	b2[0] = {3, 10};
+	b2[1] = {4, 4};
 	graph.push_back (b2);
 	
-	edge c3[3];
-	c3[0].dest = 2;
-	c3[0].wt = 10;
-	c3[1].dest = 4;
-	c3[1].wt = 7;
-	c3[2].dest = 5;
-	c3[2].wt = 1;
+	vector<edge> c3(3);
+	c3[0] = {2, 10};
+	c3[1] = {4, 7};
+	c3[2] = {5, 1};
 	graph.push_back (c3);
 
-	edge d4[3];
-	d4[0].dest = 2;
-	d4[0].wt = 10;
-	d4[1].dest = 4;
-	d4[1].wt = 7;
-	d4[2].dest = 5;
-	d4[2].wt = 1;
+	vector<edge> d4(3);
+	d4[0] = {2, 10};
+	d4[1] = {4, 7};
+	d4[2] = {5, 1};
 	graph.push_back (d4);
 	
-	edge e5[2];
-	e5[0].dest = 3;
-	e5[0].wt = 1;
-	e5[1].dest = 6;
-	e5[1].wt = 3;
+	vector<edge> e5(2);
+	e5[0] = {3, 1};
+	e5[1] = {6, 3};
 	graph.push_back (e5);
 	
-	edge f6[2];
-	f6[0].dest = 1;
-	f6[0].wt =  6;
-	f6[1].dest = 5;
-	f6[1].wt = 3;
+	vector<edge> f6(2);
+	f6[0] = {1, 6};
+	f6[1] = {5, 3};
 	graph.push_back (f6);
+	/*
+	Direct initialization approach
+	vector <vector<edge>> graph
+	{ {{4,8}, {6,6}}
+	  {{4,8}, {6,6}}
+	 .
+	 .
+	 .
+	};
+	*/
 	////////////////////////////////
 	
-	//Visted Queue
-	//Take nodes from vector and vist them 1 by 10
-	
 	//Shortest Path Array
-	//Store shortest paths to each node so farcalloc
-	//Need to overload operators to manipulations with infinity
+	//Store shortest paths to each node so far
+	//Need to overload operators for manipulations with infinity
+	//Initialize to 0 for starting point and infinity for the rest
+	int dist [graph.size()];
 	
-	cout << "test" << endl;
+	/*Initialize here*/
+	
+	//Previous node in shortest path from start node
+	int prev [graph.size()];
+	
+	for (int i = 0; i<graph.size(); i++){
+		prev[i] = -1; //let -1 be consdiered undefined
+	}
+	
+	
+	//Misc other variables for algo
+	
+	//Need change for infinity
+	int curNode = 0;
+	int testWt = 0;
+	vector<edge> curEdList;
+	
+	while (!graph.empty()){
+		//Find node closest to source
+		for (int i = 0; i < graph.size(); i++){
+			if (dist[i] < dist[curNode]) curNode = i;
+		}
+		curEdList = graph[curNode];
+		//Remove from vector to mark as visited
+		graph.erase (graph.begin()+curNode);
+		
+		//Check if path is better than existing stored path
+		for (int i = 1; i<curVertex.size(); i++){
+			testWt = dist[curNode] + curEdList[i].wt;
+			if (testWt < dist[curEdlist[i].dest]){
+				dist[curEdList[i].dest] = testWt;
+				prev[curEdList[i].dest] = curNode;
+			}
+		}
+	}
+	
+	//Reverse through the optimal path from the end node and store on stack
+	deque<int> sp;
+	curNode = end;
+	while (prev[curNode] != -1){
+		sp.push(curNode);
+		curNode = prev[curNode];
+	}
+	sp.push(curNode);
+	
+	//To get path, now pop all elements of the stack
+	//We could also add the weights, but too lazy for now, needs extra code above
+	while (!sp.empty()){
+		cout << "Path is: " << sp.top() << " ";
+		sp.pop();
+	}
+	
+	//cout << "test" << endl;
 	
 	return 0;
 
